3d7513f74066478d0b25a12f13c0a37fcb3f90f0
petd.green <petd.dev@greenlab.com>
2025-09-08 12:41:34 +0700

    fix number big

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 // server.js
 import Fastify from 'fastify';
 import { Kafka } from 'kafkajs';
+import JSONbig from 'json-bigint';
+
 
 const fastify = Fastify({ logger: true });
+
+fastify.addContentTypeParser('application/json', { parseAs: 'string' }, function (req, body, done) {
+    req.rawBody = body;
+    done(null, body);
+});
 
 // Kafka setup
 const kafka = new Kafka({ brokers: ['14.225.192.60:9094'] });
@@ -11,11 +18,11 @@ await producer.connect();
 
 // Route nhận sự kiện
 fastify.post('/webhook/order', async (request, reply) => {
-    const event = request.body;
+    const event = JSONbig.parse(request.rawBody);
     if (event) {
 
-        const shopId = event.shop_id;
-        const orderId = event.data.order_id;
+        const shopId = event.shop_id.toString;
+        const orderId = event.data.order_id.toString;
 
         const message = {
             shopId: shopId,
@@ -25,9 +32,9 @@ fastify.post('/webhook/order', async (request, reply) => {
         try {
             await producer.send({
                 topic: 'order-details-sync',
-                messages: [{ 
-                    key: message.shopId.toString(), 
-                    value: JSON.stringify(message) 
+                messages: [{
+                    key: message.shopId.toString(),
+                    value: JSON.stringify(message)
                 }],
             });
             reply.code(200).send({ message: 'Event sent to Kafka' });
@@ -39,13 +46,16 @@ fastify.post('/webhook/order', async (request, reply) => {
     }
 });
 fastify.post('/webhook/product/change', async (request, reply) => {
-  const event = request.body;
-  if (event) {
 
-        const shopId = event.shop_id;
-        const productId = event.data.product_id;
-        const status = event.data.status; 
-        const updateTime = event.data.update_time;
+    console.log("Received product change webhook:", request.rawBody);
+    const event = JSONbig.parse(request.rawBody);
+
+    if (event) {
+
+        const shopId = event.shop_id.toString();
+        const productId = event.data.product_id.toString();
+        const status = event.data.status;
+        const updateTime = event.data.update_time.toString();
 
 
         const message = {
@@ -54,13 +64,13 @@ fastify.post('/webhook/product/change', async (request, reply) => {
             event: status,
             updateTime: updateTime.toString(),
         }
-        console.log("Received product change event:", message);
+        console.log("Parsed message:", message);
         try {
             await producer.send({
                 topic: 'product-sync',
-                messages: [{ 
-                    key: message.shopId.toString(), 
-                    value: JSON.stringify(message) 
+                messages: [{
+                    key: message.shopId.toString(),
+                    value: JSON.stringify(message)
                 }],
             });
             reply.code(200).send({ message: 'Event sent to Kafka' });
@@ -75,9 +85,9 @@ fastify.post('/webhook/product/change', async (request, reply) => {
 // Start server
 const PORT = process.env.PORT || 3005;
 fastify.listen({ port: PORT, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`Server listening at ${address}`);
+    if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+    fastify.log.info(`Server listening at ${address}`);
 });
